eb46e56dbb5db895ebdbbdffb20e876c7c2fcfd8
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-16 21:41:26 -0600

    Initial commit

---
+++ ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ControllersExample.Models;
+
+namespace ControllersExample.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public ContentResult Test()
+    {
+        //return new ContentResult()
+        //{
+        //    Content = "Hello",
+        //    ContentType = "text/plain"
+        //};
+        return Content("<h1>Hello!</h1> <h2>Welcome to my world.</h2>", "text/html"); 
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
